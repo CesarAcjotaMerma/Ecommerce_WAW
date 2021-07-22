ce4b0421761ee61cbb4267b3e68cff25b4290a85
Cesar Acjota <cesar.acjota@tecsup.edu.pe>
2021-07-21 22:49:29 -0500

    actualizacion 3

--- app/Http/Livewire/OfertasComponent.php
+++ app/Http/Livewire/OfertasComponent.php
@@ -3,11 +3,20 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Sale;
+use App\Models\HomeSlider;
 
 class OfertasComponent extends Component
 {
     public function render()
     {
-        return view('livewire.ofertas-component')->layout('layouts.base');
+        $categories = Category::all();
+        $products = Product::all();
+        $sproducts = Product::where('sale_price', '>', 0)->inRandomOrder()->get()->take(8);
+        $sale = Sale::find(1);
+        $sliders = HomeSlider::where('status',1)->get();
+        return view('livewire.ofertas-component', ['products'=>$products,'categories'=>$categories, 'sproducts'=>$sproducts, 'sale'=>$sale,'sliders'=>$sliders])->layout('layouts.base');
     }
 }
