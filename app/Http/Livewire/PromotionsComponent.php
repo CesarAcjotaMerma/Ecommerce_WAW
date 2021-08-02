ceba261518088ae4e134266dc7cd01e484ccad0b
Cesar Acjota <cesar.acjota@tecsup.edu.pe>
2021-08-02 12:20:11 -0500

    Cambios de diseño y rutas

--- app/Http/Livewire/PromotionsComponent.php
+++ app/Http/Livewire/PromotionsComponent.php
@@ -14,14 +14,14 @@ class PromotionsComponent extends Component
     public function render()
     {
         // $categories = Category::all();
-        $products = Product::all();
-        $sproducts = Product::where('sale_price', '>', 0)->inRandomOrder()->get()->take(8);
-        $sale = Sale::find(1);
-        $sliders = HomeSlider::where('status',1)->get();
-        $category = HomeCategory::find(1);
-        $cats = explode(',',$category->sel_categories);
-        $categories = Category::whereIn('id',$cats)->get();
-        $no_of_products = $category->no_of_products;
+        // $products = Product::all();
+        // $sproducts = Product::where('sale_price', '>', 0)->inRandomOrder()->get()->take(8);
+        // $sale = Sale::find(1);
+        // $sliders = HomeSlider::where('status',1)->get();
+        // $category = HomeCategory::find(1);
+        // $cats = explode(',',$category->sel_categories);
+        // $categories = Category::whereIn('id',$cats)->get();
+        // $no_of_products = $category->no_of_products;
         return view('livewire.promotions-component')->layout('layouts.base');
         // ['products'=>$products,'categories'=>$categories, 'no_of_products'=>$no_of_products, 'sproducts'=>$sproducts, 'sale'=>$sale,'sliders'=>$sliders]
     }
